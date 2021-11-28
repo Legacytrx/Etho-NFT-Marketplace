0ee55662318cb9547be6e9dcb7fe213003ab0dd1
legacytrx <legacytrx@atheios.org>
2021-11-28 08:13:44 +0100

    Added comment field for display on NFT Added Navbar Added routers to have a paged design

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import CollectionContext from './store/collection-context';
 import MarketplaceContext from './store/marketplace-context'
 import NFTCollection from './abis/NFTCollection.json';
 import NFTMarketplace from './abis/NFTMarketplace.json';
+import MintForm from "./components/Content/MintNFT/MintForm";
+import logo from "./img/logo2.PNG";
+import Spinner from "./components/Layout/Spinner";
+
 
 const App = () => {
   const web3Ctx = useContext(Web3Context);
@@ -139,4 +143,289 @@ const App = () => {
   );
 };
 
-export default App;
+const Header = () => {
+  return (
+      <div>
+        <p>Header</p>
+      </div>
+  )
+};
+
+
+
+const Homepage = () => {
+  const web3Ctx = useContext(Web3Context);
+  const collectionCtx = useContext(CollectionContext);
+  const marketplaceCtx = useContext(MarketplaceContext);
+  
+  useEffect(() => {
+    // Check if the user has Metamask active
+    if(!web3) {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+      return;
+    }
+    
+    // Function to fetch all the blockchain data
+    const loadBlockchainData = async() => {
+      // Request accounts acccess if needed
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+      } catch(error) {
+        console.error(error);
+      }
+      
+      // Load account
+      const account = await web3Ctx.loadAccount(web3);
+      
+      // Load Network ID
+      const networkId = await web3Ctx.loadNetworkId(web3);
+      
+      // Load Contracts
+      const nftDeployedNetwork = NFTCollection.networks[networkId];
+      const nftContract = collectionCtx.loadContract(web3, NFTCollection, nftDeployedNetwork);
+      
+      const mktDeployedNetwork = NFTMarketplace.networks[networkId];
+      const mktContract = marketplaceCtx.loadContract(web3, NFTMarketplace, mktDeployedNetwork);
+      
+      if(nftContract) {
+        // Load total Supply
+        const totalSupply = await collectionCtx.loadTotalSupply(nftContract);
+        
+        // Load Collection
+        collectionCtx.loadCollection(nftContract, totalSupply);
+        
+        // Event subscription
+        nftContract.events.Transfer()
+            .on('data', (event) => {
+              collectionCtx.updateCollection(nftContract, event.returnValues.tokenId, event.returnValues.to);
+              collectionCtx.setNftIsLoading(false);
+            })
+            .on('error', (error) => {
+              console.log(error);
+            });
+        
+      } else {
+        window.alert('NFTCollection contract not deployed to detected network.')
+      }
+      
+      if(mktContract) {
+        // Load offer count
+        const offerCount = await marketplaceCtx.loadOfferCount(mktContract);
+        
+        // Load offers
+        marketplaceCtx.loadOffers(mktContract, offerCount);
+        
+        // Load User Funds
+        account && marketplaceCtx.loadUserFunds(mktContract, account);
+        
+        // Event OfferFilled subscription
+        mktContract.events.OfferFilled()
+            .on('data', (event) => {
+              marketplaceCtx.updateOffer(event.returnValues.offerId);
+              collectionCtx.updateOwner(event.returnValues.id, event.returnValues.newOwner);
+              marketplaceCtx.setMktIsLoading(false);
+            })
+            .on('error', (error) => {
+              console.log(error);
+            });
+        
+        // Event Offer subscription
+        mktContract.events.Offer()
+            .on('data', (event) => {
+              marketplaceCtx.addOffer(event.returnValues);
+              marketplaceCtx.setMktIsLoading(false);
+            })
+            .on('error', (error) => {
+              console.log(error);
+            });
+        
+        // Event offerCancelled subscription
+        mktContract.events.OfferCancelled()
+            .on('data', (event) => {
+              marketplaceCtx.updateOffer(event.returnValues.offerId);
+              collectionCtx.updateOwner(event.returnValues.id, event.returnValues.owner);
+              marketplaceCtx.setMktIsLoading(false);
+            })
+            .on('error', (error) => {
+              console.log(error);
+            });
+        
+      } else {
+        window.alert('ETHONFTMarketplace contract not deployed to detected network.')
+      }
+      
+      collectionCtx.setNftIsLoading(false);
+      marketplaceCtx.setMktIsLoading(false);
+      
+      // Metamask Event Subscription - Account changed
+      window.ethereum.on('accountsChanged', (accounts) => {
+        web3Ctx.loadAccount(web3);
+        accounts[0] && marketplaceCtx.loadUserFunds(mktContract, accounts[0]);
+      });
+      
+      // Metamask Event Subscription - Network changed
+      window.ethereum.on('chainChanged', (chainId) => {
+        window.location.reload();
+      });
+    };
+    
+    loadBlockchainData();
+  }, []);
+  
+  const showNavbar = web3 && collectionCtx.contract && marketplaceCtx.contract;
+  
+  return (
+  <React.Fragment>
+    {showNavbar && <Navbar />}
+    <div>
+      <h2>Test</h2>
+    </div>
+    
+  </React.Fragment>
+  );
+};
+
+const Mintpage = () => {
+  const web3Ctx = useContext(Web3Context);
+  const collectionCtx = useContext(CollectionContext);
+  const marketplaceCtx = useContext(MarketplaceContext);
+  
+  useEffect(() => {
+    // Check if the user has Metamask active
+    if(!web3) {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+      return;
+    }
+    
+    // Function to fetch all the blockchain data
+    const loadBlockchainData = async() => {
+      // Request accounts acccess if needed
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+      } catch(error) {
+        console.error(error);
+      }
+      
+      // Load account
+      const account = await web3Ctx.loadAccount(web3);
+      
+      // Load Network ID
+      const networkId = await web3Ctx.loadNetworkId(web3);
+      
+      // Load Contracts
+      const nftDeployedNetwork = NFTCollection.networks[networkId];
+      const nftContract = collectionCtx.loadContract(web3, NFTCollection, nftDeployedNetwork);
+      
+      const mktDeployedNetwork = NFTMarketplace.networks[networkId];
+      const mktContract = marketplaceCtx.loadContract(web3, NFTMarketplace, mktDeployedNetwork);
+      
+      if(nftContract) {
+        // Load total Supply
+        const totalSupply = await collectionCtx.loadTotalSupply(nftContract);
+        
+        // Load Collection
+        collectionCtx.loadCollection(nftContract, totalSupply);
+        
+        // Event subscription
+        nftContract.events.Transfer()
+            .on('data', (event) => {
+              collectionCtx.updateCollection(nftContract, event.returnValues.tokenId, event.returnValues.to);
+              collectionCtx.setNftIsLoading(false);
+            })
+            .on('error', (error) => {
+              console.log(error);
+            });
+        
+      } else {
+        window.alert('NFTCollection contract not deployed to detected network.')
+      }
+      
+      if(mktContract) {
+        // Load offer count
+        const offerCount = await marketplaceCtx.loadOfferCount(mktContract);
+        
+        // Load offers
+        marketplaceCtx.loadOffers(mktContract, offerCount);
+        
+        // Load User Funds
+        account && marketplaceCtx.loadUserFunds(mktContract, account);
+        
+        // Event OfferFilled subscription
+        mktContract.events.OfferFilled()
+            .on('data', (event) => {
+              marketplaceCtx.updateOffer(event.returnValues.offerId);
+              collectionCtx.updateOwner(event.returnValues.id, event.returnValues.newOwner);
+              marketplaceCtx.setMktIsLoading(false);
+            })
+            .on('error', (error) => {
+              console.log(error);
+            });
+        
+        // Event Offer subscription
+        mktContract.events.Offer()
+            .on('data', (event) => {
+              marketplaceCtx.addOffer(event.returnValues);
+              marketplaceCtx.setMktIsLoading(false);
+            })
+            .on('error', (error) => {
+              console.log(error);
+            });
+        
+        // Event offerCancelled subscription
+        mktContract.events.OfferCancelled()
+            .on('data', (event) => {
+              marketplaceCtx.updateOffer(event.returnValues.offerId);
+              collectionCtx.updateOwner(event.returnValues.id, event.returnValues.owner);
+              marketplaceCtx.setMktIsLoading(false);
+            })
+            .on('error', (error) => {
+              console.log(error);
+            });
+        
+      } else {
+        window.alert('ETHONFTMarketplace contract not deployed to detected network.')
+      }
+      
+      collectionCtx.setNftIsLoading(false);
+      marketplaceCtx.setMktIsLoading(false);
+      
+      // Metamask Event Subscription - Account changed
+      window.ethereum.on('accountsChanged', (accounts) => {
+        web3Ctx.loadAccount(web3);
+        accounts[0] && marketplaceCtx.loadUserFunds(mktContract, accounts[0]);
+      });
+      
+      // Metamask Event Subscription - Network changed
+      window.ethereum.on('chainChanged', (chainId) => {
+        window.location.reload();
+      });
+    };
+    
+    loadBlockchainData();
+  }, []);
+  
+  const showNavbar = web3 && collectionCtx.contract && marketplaceCtx.contract;
+  
+  return (
+      <React.Fragment>
+        {showNavbar && <Navbar />}
+        <div>
+          <div className="row">
+            <main role="main" className="col-lg-12 justify-content-center text-center">
+              <div className="content mr-auto ml-auto">
+                <img src={logo} alt="logo" width="500" height="140" className="mb-2"/>
+                {!collectionCtx.nftIsLoading && <MintForm />}
+                {collectionCtx.nftIsLoading && <Spinner />}
+              </div>
+            </main>
+          </div>
+          <hr/>
+  
+        </div>
+      
+      </React.Fragment>
+  );
+};
+
+export {Homepage, Mintpage, Header , App} ; //new
+
--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -4,6 +4,7 @@ import Web3Context from '../../store/web3-context';
 import MarketplaceContext from '../../store/marketplace-context';
 import web3 from '../../connection/web3';
 import { formatPrice } from '../../helpers/utils';
+import eth from "../../img/eth.png";
 
 const Navbar = () => {
   const [fundsLoading, setFundsLoading] = useState(false);
@@ -44,57 +45,72 @@ const Navbar = () => {
     console.log(error);
   });
 
-  let etherscanUrl;
-
-  if(web3Ctx.networkId === 3) {
-    etherscanUrl = 'https://ropsten.etherscan.io'
-  } else if(web3Ctx.networkId === 4) {
-    etherscanUrl = 'https://rinkeby.etherscan.io'
-  } else if(web3Ctx.networkId === 5) {
-    etherscanUrl = 'https://goerli.etherscan.io'
-  } else {
-    etherscanUrl = 'https://etherscan.io'
-  }
   
   return (
-    <nav className="navbar navbar-expand-sm navbar-light bg-white p-0">      
-      <ul className="navbar-nav ms-auto">
-        <li className="nav-item">
-          {marketplaceCtx.userFunds > 0 && !fundsLoading &&
-            <button 
-              type="button" 
-              className="btn btn-info btn-block navbar-btn text-white" 
-              onClick={claimFundsHandler}
-            >          
-              {`YOU SOLD AN ITEM! CLAIM ${formatPrice(marketplaceCtx.userFunds)} ETHO`}
-            </button>}
-          {fundsLoading &&
-            <div class="d-flex justify-content-center text-info">
-              <div class="spinner-border" role="status">
-                <span class="sr-only"></span>
-              </div>
-          </div>}          
-        </li>
-        <li className="nav-item">
-          {web3Ctx.account && 
-            <a 
-              className="nav-link small" 
-              href={`https://explorer.ethoprotocol.com/address/${web3Ctx.account}`}
-              target="blank"
-              rel="noopener noreferrer"
-            >
-              {web3Ctx.account}
-            </a>}
-          {!web3Ctx.account && 
-            <button 
-              type="button" 
-              className="btn btn-info text-white" 
-              onClick={connectWalletHandler} 
-            > 
-              Connect your wallet
-            </button>}
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-sm navbar-light bg-light p-0">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <img src={eth} alt="" width="64" height="64"></img>
+        </a>
+  
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="Explore">Explore</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="mint">Mint</a>
+            </li>
+  
+            <li className="nav-item">
+              <a className="nav-link disabled" href="/">Disabled</a>
+            </li>
+          <li className="nav-item">
+            {marketplaceCtx.userFunds > 0 && !fundsLoading &&
+              <button
+                type="button"
+                className="btn btn-info btn-block navbar-btn text-white"
+                onClick={claimFundsHandler}
+              >
+                {`YOU SOLD AN ITEM! CLAIM ${formatPrice(marketplaceCtx.userFunds)} ETHO`}
+              </button>}
+            {fundsLoading &&
+              <div class="d-flex justify-content-center text-info">
+                <div class="spinner-border" role="status">
+                  <span class="sr-only"></span>
+                </div>
+            </div>}
+          </li>
+          <li className="nav-item">
+            {web3Ctx.account &&
+              <a
+                className="nav-link small"
+                href={`https://explorer.ethoprotocol.com/address/${web3Ctx.account}`}
+                target="blank"
+                rel="noopener noreferrer"
+              >
+                {web3Ctx.account}
+              </a>}
+            {!web3Ctx.account &&
+              <button
+                type="button"
+                className="btn btn-info text-white"
+                onClick={connectWalletHandler}
+              >
+                Connect your wallet
+              </button>}
+          </li>
+        </ul>
+      </div>
+        </div>
     </nav>
   );  
 };
